fc4fbc3331aee2a5df40101a83e425f82a825170
Yerson Alexander Arredondo García <yerson-302@hotmail.com>
2025-09-06 21:48:02 -0500

    fix: adjust grid layout in MainLayout component for improved responsiveness

--- src/layouts/main.tsx
+++ src/layouts/main.tsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router";
 
 export const MainLayout = () => {
   return (
-    <main className="h-screen grid grid-cols-2 grid-rows-2 grid-flow-row-dense">
+    <main className="h-screen grid grid-cols-1 grid-rows-1">
       <section className="grid grid-cols-[18rem_1fr] h-full w-full">
         <Sidebar />
         <div className="p-4 overflow-y-auto overscroll-contain">
