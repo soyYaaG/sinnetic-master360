4dcf7b150b6938eb5faf887ba0f691fff085aeb4
Yerson Alexander Arredondo García <yerson-302@hotmail.com>
2025-09-07 00:07:40 -0500

    feat: enhance Sidebar component with navigation links and structure

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,11 +1,21 @@
 import { SiBrandfolder } from "react-icons/si";
 import { IoCreateOutline } from "react-icons/io5";
 import { PiEyeThin } from "react-icons/pi";
+import { Link } from "react-router";
+import { FiHome } from "react-icons/fi";
+import { LuLayers3 } from "react-icons/lu";
+import { VscSettings } from "react-icons/vsc";
 
 export const Sidebar = () => {
   return (
     <nav className="bg-base-200 w-72 overflow-y-auto overscroll-contain pr-1">
       <ul className="menu rounded-box max-w-xs w-full">
+        <li>
+          <Link to="/">
+            <FiHome />
+            Principal
+          </Link>
+        </li>
         <li>
           <details>
             <summary>
@@ -27,6 +37,36 @@ export const Sidebar = () => {
               </li>
             </ul>
           </details>
+        </li>
+
+        <li>
+          <details>
+            <summary>
+              <LuLayers3 />
+              Catalogo
+            </summary>
+            <ul>
+              <li>
+                <Link to="/catalog/create">
+                  <IoCreateOutline />
+                  Crear
+                </Link>
+              </li>
+              <li>
+                <a>
+                  <PiEyeThin />
+                  Ver Catalogo
+                </a>
+              </li>
+            </ul>
+          </details>
+        </li>
+
+        <li>
+          <Link to="/setting">
+            <VscSettings />
+            Configuración
+          </Link>
         </li>
       </ul>
     </nav>
